c0cd4e1cf293dcd2787a22640f0de0a9632c1157
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-08-08 19:34:17 -0300

    Create caso1.java

---
+++ caso1.java
@@ -0,0 +1,13 @@
+package casos;
+
+public class caso1
+{	
+  public static void main(String[] args) 
+  {
+	int a=5;
+	int b=10;
+	int suma=0;
+	suma=a+b;		
+	System.out.println("La suma de a y b es: "+ suma);
+  }
+}
