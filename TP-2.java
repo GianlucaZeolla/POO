20afb44e05eb72ffc88723911a383f62f70e9852
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-08-28 16:24:38 -0300

    Update TP-2.java

--- TP-2.java
+++ TP-2.java
@@ -1,4 +1,5 @@
-import java.io.ObjectOutput;
+package aa;
+
 import java.util.Scanner;
 
 public class Alumno {
@@ -12,16 +13,16 @@ public class Alumno {
         this.promedio = promedio;
     }
 
-    public Alumno(String juan, int edad, double v) {
+    public Alumno(String juan, int edad, double promedio) {
 
     }
 
-    public String getNombre() {
-        return getNombre();
+    public  String getNombre() {
+        return nombre;
     }
 
-    public int getEdad() {
-        return getEdad();
+	public int getEdad() {
+        return edad;
     }
 
     public double[] getPromedio() {
@@ -39,29 +40,27 @@ public class Alumno {
     public void setPromedio(double[] promedio) {
         return;
     }
-
-
+    
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Creando el objeto alumno1....");
-        System.out.println("¿desea continuar? Ingrese 1 para si, 0 para no");
-        int deseoContinuar = teclado.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Creando el objeto alumno....");
+        System.out.println("¿desea continuar? Ingrese 1 para si, 2 para no");
+        int deseoContinuar = scanner.nextInt();
 
         if (deseoContinuar == 1) {
             System.out.println("ingreso");
+            Alumno alu1 = new Alumno("juan", 23,new double[] {8.25});
+        
+        System.out.println("atributos de alu1: ");
+        System.out.println("nombre: " + alu1.getNombre());
+        System.out.println("edad: " + alu1.getEdad());
+        System.out.println("promedio: " + alu1.getPromedio()[0]);
         } else {
             System.out.println("que tenga buen dia");
         }
-        Alumno alu1 = new Alumno("juan", 23, 8.25);
-        System.out.println("atributos de alu1: ");
-        System.out.println("nombre: " + alu1.nombre);
-        System.out.println("edad: " + alu1.getEdad());
-        System.out.println("promedio: " + alu1.getPromedio());
-
-
-
-
-
+        
+        
 
         }
     }
